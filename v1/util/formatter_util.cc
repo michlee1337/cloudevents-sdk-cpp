b53983f4cfa0447672cf18bb9010104ad76768db
michlee1337 <michlee1337@gmail.com>
2020-07-30 17:15:51 +0000

    setup

---
+++ v1/util/formatter_util.cc
@@ -0,0 +1,11 @@
+#include "formatter_util.h"
+
+namespace cloudevents {
+namespace util {
+
+using ::cloudevents::format::Format;
+using ::cloudevents::format::Formatter;
+using ::cloudevents::format::JsonFormatter;
+
+} // util
+} // cloudevents
