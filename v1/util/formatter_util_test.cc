e1c99b54e11473181e2b4ee57564f59ef58b6ab9
michlee1337 <michlee1337@gmail.com>
2020-07-30 17:15:51 +0000

    setup

---
+++ v1/util/formatter_util_test.cc
@@ -0,0 +1,13 @@
+#include "formatter_util.h"
+
+#include <gtest/gtest.h>
+
+namespace cloudevents {
+namespace util {
+
+using ::cloudevents::format::Format;
+using ::cloudevents::format::Formatter;
+using ::cloudevents::format::JsonFormatter;
+
+} // util
+} // cloudevents
