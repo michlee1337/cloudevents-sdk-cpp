bfb09d1fad45f8584c14018469337dedfbe66f54
michlee1337 <michlee1337@gmail.com>
2020-07-30 17:16:26 +0000

    DestringifyFormat impl

--- v1/util/formatter_util.cc
+++ v1/util/formatter_util.cc
@@ -7,5 +7,12 @@ using ::cloudevents::format::Format;
 using ::cloudevents::format::Formatter;
 using ::cloudevents::format::JsonFormatter;
 
+absl::StatusOr<Format> FormatterUtil::DestringifyFormat(std::string format_str) {
+    if (format_str == "json") {
+        return Format::kJson;
+    }
+    return absl::InvalidArgumentError("The given format is not currently recognized by the SDK.");
+}
+
 } // util
 } // cloudevents
