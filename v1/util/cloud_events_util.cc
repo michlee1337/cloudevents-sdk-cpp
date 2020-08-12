063a35f19dd2e113722cdca189359e0de4139b7e
michlee1337 <michlee1337@gmail.com>
2020-08-12 02:20:13 +0000

    all error strings at top of file

--- v1/util/cloud_events_util.cc
+++ v1/util/cloud_events_util.cc
@@ -10,12 +10,17 @@ using ::io::cloudevents::v1::CloudEvent_CloudEventAttribute;
 using ::google::protobuf::Timestamp;
 using ::google::protobuf::util::TimeUtil;
 
+constexpr char kErrCeInvalid[] = "Given Cloud Event is missing required attributes.";
+constexpr char kErrTimeInvalid[] = "Given time is invalid because it does not comply to RFC 3339.";
+constexpr char kErrAttrNotSet[] = "Given Cloud Event attribute is not set.";
+constexpr char kErrAttrNotHandled[] = "A Cloud Event type is not handled.";
+
 absl::Status CloudEventsUtil::IsValid(const CloudEvent& cloud_event) {
     if (cloud_event.id().empty() ||
             cloud_event.source().empty() ||
             cloud_event.spec_version().empty() ||
             cloud_event.type().empty()) {
-        return absl::InvalidArgumentError("Given Cloud Event is missing required attributes.");
+        return absl::InvalidArgumentError(kErrCeInvalid);
     }
     return absl::OkStatus();
 }
@@ -57,7 +62,7 @@ absl::Status CloudEventsUtil::SetMetadata(const std::string& key,
         CloudEvent_CloudEventAttribute attr;
         Timestamp timestamp;
         if (!TimeUtil::FromString(val, &timestamp)) {
-            return absl::InvalidArgumentError("Time given is invalid because it does not comply to RFC 3339.");
+            return absl::InvalidArgumentError(kErrTimeInvalid);
         }
         (*attr.mutable_ce_timestamp()) = timestamp;
         (*cloud_event.mutable_attributes())[key] = attr;
@@ -70,37 +75,30 @@ absl::Status CloudEventsUtil::SetMetadata(const std::string& key,
     return absl::OkStatus();
 }
 
-cloudevents_absl::StatusOr<std::string> CloudEventsUtil::ToString(
-    const CloudEvent_CloudEventAttribute& attr){
-  switch (attr.attr_oneof_case()) {
-    case CloudEvent_CloudEventAttribute::AttrOneofCase::kCeBoolean:
-      // StatusOr requires explicit conversion
-      return std::string(attr.ce_boolean() ? "true" : "false");
-    case CloudEvent_CloudEventAttribute::AttrOneofCase::kCeInteger:
-      // skipping validity checks as protobuf generates int32 for sfixed32
-      return std::to_string(attr.ce_integer());
-    case CloudEvent_CloudEventAttribute::AttrOneofCase::kCeString:
-      return attr.ce_string();
-    case CloudEvent_CloudEventAttribute::AttrOneofCase::kCeBinary:
-      return cloudevents_base64::base64_encode(attr.ce_binary());
-    case CloudEvent_CloudEventAttribute::AttrOneofCase::kCeUri:
-      return attr.ce_uri();
-    case CloudEvent_CloudEventAttribute::AttrOneofCase::kCeUriReference:
-      return attr.ce_uri_reference();
-    case CloudEvent_CloudEventAttribute::AttrOneofCase::kCeTimestamp:
-      // protobuf also uses RFC3339 representation
-      return TimeUtil::ToString(attr.ce_timestamp());
-    case CloudEvent_CloudEventAttribute::AttrOneofCase::ATTR_ONEOF_NOT_SET:
-      return absl::InvalidArgumentError(kErrAttrNotSet);
-  }
-  return absl::InternalError(kErrAttrNotHandled);
-}
-
-CloudEvent_CloudEventAttribute CloudEventsUtil::ToCeString(
-    const std::string& val) {
-  CloudEvent_CloudEventAttribute ce_str;
-  ce_str.set_ce_string(val);
-  return ce_str;
+absl::StatusOr<std::string> CloudEventsUtil::StringifyCeType(
+        const CloudEvent_CloudEventAttribute& attr){
+    switch (attr.attr_oneof_case()) {
+        case CloudEvent_CloudEventAttribute::AttrOneofCase::kCeBoolean:
+            // StatusOr requires explicit conversion
+            return std::string(attr.ce_boolean() ? "true" : "false");
+        case CloudEvent_CloudEventAttribute::AttrOneofCase::kCeInteger:
+            // skipping validity checks as protobuf generates int32 for sfixed32
+            return std::to_string(attr.ce_integer());
+        case CloudEvent_CloudEventAttribute::AttrOneofCase::kCeString:
+            return attr.ce_string();
+        case CloudEvent_CloudEventAttribute::AttrOneofCase::kCeBinary:
+            return base64::base64_encode(attr.ce_binary());
+        case CloudEvent_CloudEventAttribute::AttrOneofCase::kCeUri:
+            return attr.ce_uri();
+        case CloudEvent_CloudEventAttribute::AttrOneofCase::kCeUriReference:
+            return attr.ce_uri_reference();
+        case CloudEvent_CloudEventAttribute::AttrOneofCase::kCeTimestamp:
+            // protobuf also uses RFC3339 representation
+            return TimeUtil::ToString(attr.ce_timestamp());
+        case CloudEvent_CloudEventAttribute::AttrOneofCase::ATTR_ONEOF_NOT_SET:
+            return absl::InvalidArgumentError(kErrAttrNotSet);
+    }
+    return absl::InternalError(kErrAttrNotHandled);
 }
 
 }  // namespace cloudevents_util
