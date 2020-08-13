fb1c3ed6f92a4358050a55cd697cab49443b1fc8
michlee1337 <michlee1337@gmail.com>
2020-08-13 00:52:14 +0000

    rename and return StatusOr

--- v1/util/formatter_util_test.cc
+++ v1/util/formatter_util_test.cc
@@ -9,26 +9,28 @@ using ::cloudevents::format::Format;
 using ::cloudevents::format::Formatter;
 using ::cloudevents::format::JsonFormatter;
 
-TEST(FormatterUtilTest, DestringifyFormat_Json) {
-    absl::StatusOr<Format> format_str = FormatterUtil::DestringifyFormat("json");
-    ASSERT_TRUE(format_str.ok());
-    ASSERT_EQ((*format_str), Format::kJson);
+TEST(FormatterUtilTest, FormatFromString_Json) {
+  absl::StatusOr<Format> format = FormatterUtil::FormatFromString("json");
+  ASSERT_TRUE(format.ok());
+  ASSERT_EQ(*format, Format::kJson);
 }
 
-TEST(FormatterUtilTest, DestringifyFormat_Invalid) {
-    ASSERT_TRUE(absl::IsInvalidArgument(
-        FormatterUtil::DestringifyFormat("not a format").status()));
+TEST(FormatterUtilTest, FormatFromString_Invalid) {
+  ASSERT_TRUE(absl::IsInvalidArgument(
+    FormatterUtil::FormatFromString("not a format").status()));
 }
 
-TEST(FormatterUtilTest, StringifyFormat_Json) {
-    ASSERT_EQ(FormatterUtil::StringifyFormat(Format::kJson), "json");
+TEST(FormatterUtilTest, FormatToString_Json) {
+  absl::StatusOr<std::string> format_str = FormatterUtil::FormatToString(Format::kJson);
+  ASSERT_TRUE(format_str.ok());
+  ASSERT_EQ(*format_str, "json");
 }
 
 TEST(FormatterUtilTest, GetFormatter_Json) {
-    absl::StatusOr<std::unique_ptr<Formatter>> get_formatter;
-    get_formatter = FormatterUtil::GetFormatter(Format::kJson);
-    ASSERT_TRUE(get_formatter.ok());
-    ASSERT_EQ(typeid(*(*(get_formatter))), typeid(JsonFormatter));  // dereference twice
+  absl::StatusOr<std::unique_ptr<Formatter>> get_formatter;
+  get_formatter = FormatterUtil::GetFormatter(Format::kJson);
+  ASSERT_TRUE(get_formatter.ok());
+  ASSERT_EQ(typeid(**get_formatter), typeid(JsonFormatter));  // dereference twice
 }
 
 } // util
