860bb6ee8317ebfad0b723022c6954ae32c5b3fc
michlee1337 <michlee1337@gmail.com>
2020-08-13 00:52:14 +0000

    rename and return StatusOr

--- v1/util/formatter_util.cc
+++ v1/util/formatter_util.cc
@@ -7,30 +7,35 @@ using ::cloudevents::format::Format;
 using ::cloudevents::format::Formatter;
 using ::cloudevents::format::JsonFormatter;
 
-absl::StatusOr<Format> FormatterUtil::DestringifyFormat(
-        const std::string& format_str) {
-    if (format_str == "json") {
-        return Format::kJson;
-    }
-    return absl::InvalidArgumentError("The given format is not currently recognized by the SDK.");
+constexpr char kErrUnkFormat[] = "A Format has not been handled.";
+constexpr char kErrUnkFormatStr[] = "The given format is not recognized by the SDK.";
+constexpr char kErrUnkFormatter[] = "Could not find formatter for given format.";
+
+absl::StatusOr<Format> FormatterUtil::FormatFromString(
+    const std::string& format_str) {
+  if (format_str == "json") {
+    return Format::kJson;
+  }
+  return absl::InvalidArgumentError(kErrUnkFormatStr);
 }
 
-std::string FormatterUtil::StringifyFormat(const Format& format) {
-    switch (format) {
-        case Format::kJson:
-            return std::string("json");
-    }
+absl::StatusOr<std::string> FormatterUtil::FormatToString(
+    const Format& format) {
+  switch (format) {
+    case Format::kJson:
+      return std::string("json");
+  }
+  return absl::InternalError(kErrUnkFormat);
 }
 
-absl::StatusOr<
-        std::unique_ptr<Formatter>> 
-        FormatterUtil::GetFormatter(const Format& format) {
-    switch (format) {
-        case Format::kJson:
-            auto m = new JsonFormatter;
-            return std::unique_ptr<Formatter>(m);
-    }
-    return absl::InternalError("Could not find formatter for given format.");
+absl::StatusOr<std::unique_ptr<Formatter>> FormatterUtil::GetFormatter(
+    const Format& format) {
+  switch (format) {
+    case Format::kJson:
+      auto m = new JsonFormatter;
+      return std::unique_ptr<Formatter>(m);
+  }
+  return absl::InternalError(kErrUnkFormatter);
 }
 
 } // util
