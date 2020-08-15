39c75743ddb62095fdc66dc06dbb7742cdf3eacb
michlee1337 <michlee1337@gmail.com>
2020-08-15 00:40:42 +0000

    refactor binary bind/unbind to smaller funcs

--- v1/protocol_binding/http_binder.cc
+++ v1/protocol_binding/http_binder.cc
@@ -14,108 +14,176 @@ using ::cloudevents::cloudevents_util::CloudEventsUtil;
 typedef boost::beast::http::request<boost::beast::http::string_body> HttpRequest;
 typedef absl::flat_hash_map<std::string, CloudEvent_CloudEventAttribute> CeAttrMap;
 
-constexpr absl::string_view kHttpContentKey = "Content-Type";
+static const std::string kHttpContentKey = "Content-Type";
+
+// _____ Operations used in Unbind Structured _____
 
 template <>
-absl::StatusOr<std::string> Binder<HttpRequest>::GetContentType(HttpRequest& http_req) {
-    auto iter = http_req.base().find(kHttpContentKey.data());
-    if (iter == http_req.base().end()) {
-        return std::string("");
-    }
-    return std::string(iter->value());
+absl::StatusOr<std::string> Binder<HttpRequest>::GetContentType(const HttpRequest& http_req) {
+  auto iter = http_req.base().find(kHttpContentKey.data());
+  if (iter == http_req.base().end()) {
+    return std::string("");
+  }
+  return std::string(iter->value());
 }
 
 template <>
-absl::StatusOr<std::string> Binder<HttpRequest>::GetPayload(HttpRequest& http_req) {
-    return http_req.body();
+absl::StatusOr<std::string> Binder<HttpRequest>::GetPayload(const HttpRequest& http_req) {
+  return http_req.body();
+}
+
+// template <>
+// absl::StatusOr<CloudEvent> Binder<HttpRequest>::UnbindBinary(HttpRequest& http_req) {
+//   CloudEvent cloud_event;
+//   for (auto it = http_req.base().begin(); it!=http_req.base().end(); ++it) {
+//     std::string key;
+//     std::string header_key = (*it).name_string().to_string();
+//     if (header_key == kHttpContentKey.data()) {
+//       key = kContenttypeKey.data();
+//     } 
+//     else if (header_key.rfind(kMetadataPrefix.data(), 0) == 0){
+//       size_t len_prefix = strlen(kMetadataPrefix.data());
+//       key = header_key.substr(len_prefix, std::string::npos);
+//     }
+//     CloudEventsUtil::SetMetadata(cloud_event, key, (*it).value().to_string());
+//   }
+
+//   std::string http_data = http_req.body();
+//   if (!http_data.empty()) {
+//     cloud_event.set_binary_data(http_data);
+//   }
+  
+//   if (!CloudEventsUtil::IsValid(cloud_event)) {
+//     return absl::InvalidArgumentError("Pubsub Message given does not contain a valid binary Cloud Event");
+//   }
+//   return cloud_event;
+// }
+
+// _____ Operations used in Unbind Binary _____
+
+template <>
+absl::Status Binder<HttpRequest>::UnbindMetadata(
+    const HttpRequest& http_req, CloudEvent& cloud_event) {
+  for (auto it = http_req.base().begin(); it!=http_req.base().end(); ++it) {
+    std::string header_key = (*it).name_string().to_string();
+    std::string header_val = (*it).value().to_string();
+    if (header_key == kHttpContentKey) {
+      if (auto set_metadata = CloudEventsUtil::SetMetadata(kContenttypeKey,
+          header_val, cloud_event); !set_metadata.ok()) {
+        return set_metadata;
+      }  
+    } 
+    else if (header_key.rfind(kMetadataPrefix, 0) == 0){
+      size_t len_prefix = kMetadataPrefix.length();
+      std::string key = header_key.substr(len_prefix, std::string::npos);
+      if (auto set_metadata = CloudEventsUtil::SetMetadata(key,
+          header_val, cloud_event); !set_metadata.ok()) {
+        return set_metadata;
+      }  
+    }
+  }
+  return absl::OkStatus();
 }
 
 template <>
-absl::StatusOr<CloudEvent> Binder<HttpRequest>::UnbindBinary(HttpRequest& http_req) {
-    CloudEvent cloud_event;
-    for (auto it = http_req.base().begin(); it!=http_req.base().end(); ++it) {
-        std::string key;
-        std::string header_key = (*it).name_string().to_string();
-        if (header_key == kHttpContentKey.data()) {
-            key = kContenttypeKey.data();
-        } 
-        else if (header_key.rfind(kMetadataPrefix.data(), 0) == 0){
-            size_t len_prefix = strlen(kMetadataPrefix.data());
-            key = header_key.substr(len_prefix, std::string::npos);
-        }
-        CloudEventsUtil::SetMetadata(cloud_event, key, (*it).value().to_string());
-    }
+absl::Status Binder<HttpRequest>::UnbindData(
+    const HttpRequest& http_req, CloudEvent& cloud_event) {
+  if (!http_req.body().empty()) {
+    cloud_event.set_binary_data(http_req.body());
+  }
+  return absl::OkStatus();
+}
 
-    std::string http_data = http_req.body();
-    if (!http_data.empty()) {
-        cloud_event.set_binary_data(http_data);
-    }
-    
-    if (!CloudEventsUtil::IsValid(cloud_event)) {
-        return absl::InvalidArgumentError("Pubsub Message given does not contain a valid binary Cloud Event");
-    }
-    return cloud_event;
+// _____ Operations used in Bind Structured _____
+
+template <>
+absl::Status Binder<HttpRequest>::BindContentType(
+    const std::string& contenttype, HttpRequest& http_req) {
+  http_req.base().set(kHttpContentKey.data(), contenttype);
+  return absl::OkStatus();
 }
 
 template <>
-absl::Status Binder<HttpRequest>::SetContentType(HttpRequest& http_req,
-        std::string contenttype) {
-    http_req.base().set(kHttpContentKey.data(), contenttype);
-    return absl::Status();
+absl::Status Binder<HttpRequest>::BindDataStructured(
+    const std::string& payload, HttpRequest& http_req) {
+  http_req.body() = payload;
+  return absl::OkStatus();
+}
+
+// template <>
+// absl::StatusOr<HttpRequest> Binder<HttpRequest>::BindBinary(CloudEvent& cloud_event) {
+//   if (!CloudEventsUtil::IsValid(cloud_event)) {
+//     return absl::InvalidArgumentError("Cloud Event given is not valid.");
+//   }
+
+//   HttpRequest http_req;
+
+//   absl::StatusOr<CeAttrMap> attrs;
+//   attrs = CloudEventsUtil::GetMetadata(cloud_event);
+//   if (!attrs.ok()) {
+//     return attrs.status();
+//   }
+
+//   for (auto const& attr : (*attrs)) {
+//     absl::StatusOr<std::string> val = CloudEventsUtil::ToString(attr.second);
+//     if (!val.ok()) {
+//       return val.status();
+//     }
+//     std::string key = k(kMetadataPrefix + attr.first;
+//     http_req.base().setkey,*val);
+//   }
+
+//   // http binding spec states to use the body should carry the data as is
+//   std::string data;
+//   switch (cloud_event.data_oneof_case()) {
+//     case CloudEvent::DataOneofCase::kBinaryData: {
+//       data = cloud_event.binary_data();
+//       break;
+//     }
+//     case CloudEvent::DataOneofCase::kTextData: {
+//       data = cloud_event.text_data();
+//       break;
+//     }
+//     case CloudEvent::DataOneofCase::kProtoData: {
+//       // TODO (#17): Handle CloudEvent Any in JsonFormatter
+//       return absl::UnimplementedError("protobuf::Any not supported yet.");
+//     }
+//     case CloudEvent::DATA_ONEOF_NOT_SET: {
+//       break;
+//     }
+//   }
+
+//   http_req.body() = data;
+//   return http_req;
+// }
+
+// _____ Operations used in Bind Binary _____
+
+template <>
+absl::Status Binder<HttpRequest>::BindMetadata(const std::string& key,
+    const CloudEvent_CloudEventAttribute& val, HttpRequest& http_req) {
+  absl::StatusOr<std::string> val_str = CloudEventsUtil::ToString(val);
+  if (!val_str.ok()) {
+    return val_str.status();
+  }
+  http_req.base().set(key, *val_str);
+  return absl::OkStatus();
 }
 
 template <>
-absl::Status Binder<HttpRequest>::SetPayload(HttpRequest& http_req,
-        std::string payload) {
-    http_req.body() = payload;
-    return absl::Status();
+absl::Status Binder<HttpRequest>::BindDataBinary(const std::string& bin_data,
+    HttpRequest& http_req) {
+  // spec states to place data into body as is
+  http_req.body() = bin_data;
+  return absl::OkStatus();
 }
 
 template <>
-absl::StatusOr<HttpRequest> Binder<HttpRequest>::BindBinary(CloudEvent& cloud_event) {
-    if (!CloudEventsUtil::IsValid(cloud_event)) {
-        return absl::InvalidArgumentError("Cloud Event given is not valid.");
-    }
-
-    HttpRequest http_req;
-
-    absl::StatusOr<CeAttrMap> attrs;
-    attrs = CloudEventsUtil::GetMetadata(cloud_event);
-    if (!attrs.ok()) {
-        return attrs.status();
-    }
-
-    for (auto const& attr : (*attrs)) {
-        absl::StatusOr<std::string> val = CloudEventsUtil::StringifyCeType(attr.second);
-        if (!val.ok()) {
-            return val.status();
-        }
-        std::string key = kMetadataPrefix.data() + attr.first;
-        http_req.base().set(key,*val);
-    }
-
-    // http binding spec states to use the body should carry the data as is
-    std::string data;
-    switch (cloud_event.data_oneof_case()) {
-        case CloudEvent::DataOneofCase::kBinaryData: {
-            data = cloud_event.binary_data();
-            break;
-        }
-        case CloudEvent::DataOneofCase::kTextData: {
-            data = cloud_event.text_data();
-            break;
-        }
-        case CloudEvent::DataOneofCase::kProtoData: {
-            // TODO (#17): Handle CloudEvent Any in JsonFormatter
-            return absl::UnimplementedError("protobuf::Any not supported yet.");
-        }
-        case CloudEvent::DATA_ONEOF_NOT_SET: {
-            break;
-        }
-    }
-
-    http_req.body() = data;
-    return http_req;
+absl::Status Binder<HttpRequest>::BindDataText(const std::string& text_data,
+    HttpRequest& http_req) {
+  // spec states to place data into body as is
+  http_req.body() = text_data;
+  return absl::OkStatus();
 }
 
 } // binding
--- v1/protocol_binding/http_binder.h
+++ v1/protocol_binding/http_binder.h
@@ -7,40 +7,86 @@
 namespace cloudevents {
 namespace binding {
 
-typedef boost::beast::http::request<boost::beast::http::string_body> HttpRequest;
+/*
+ * Template specialization to handle HTTPRequest and HTTPResponse.
+ * For the full template code and comments,
+ * view `//v1/protocol_binding/binder.h`.
+ *
+ * uses Boost beast's HTTP Request/ Response representation.
+ */
 
-// HTTP specialization
+// _____ Operations used in Unbind Structured _____
 template <>
 absl::StatusOr<std::string> 
-    Binder<boost::beast::http::request<boost::beast::http::string_body>>::
-    GetContentType(HttpRequest& http_msg);
+  Binder<boost::beast::http::request<boost::beast::http::string_body>>::
+  GetContentType(
+  const boost::beast::http::request<boost::beast::http::string_body>& http_req);
 
 template <>
 absl::StatusOr<std::string> 
-    Binder<boost::beast::http::request<boost::beast::http::string_body>>::GetPayload(
-    boost::beast::http::request<boost::beast::http::string_body>& pubsub_msg);
+  Binder<boost::beast::http::request<boost::beast::http::string_body>>::
+  GetPayload(
+  const boost::beast::http::request<boost::beast::http::string_body>& http_req);
+
+// _____ Operations used in Unbind Binary _____
+
+// template <>
+// absl::StatusOr<io::cloudevents::v1::CloudEvent> 
+//   Binder<boost::beast::http::request<boost::beast::http::string_body>>::UnbindBinary(
+//   boost::beast::http::request<boost::beast::http::string_body>& http_req);
 
 template <>
-absl::StatusOr<io::cloudevents::v1::CloudEvent> 
-    Binder<boost::beast::http::request<boost::beast::http::string_body>>::UnbindBinary(
-    boost::beast::http::request<boost::beast::http::string_body>& pubsub_msg);
+absl::Status
+  Binder<boost::beast::http::request<boost::beast::http::string_body>>::
+  UnbindMetadata(
+  const boost::beast::http::request<boost::beast::http::string_body>& http_req,
+  io::cloudevents::v1::CloudEvent& cloud_event);
+
+template <>
+absl::Status
+  Binder<boost::beast::http::request<boost::beast::http::string_body>>::
+  UnbindData(
+  const boost::beast::http::request<boost::beast::http::string_body>& http_req,
+  io::cloudevents::v1::CloudEvent& cloud_event);
+
+// _____ Operations used in Bind Structured _____
 
 template <>
 absl::Status 
-    Binder<boost::beast::http::request<boost::beast::http::string_body>>::SetContentType(
-    boost::beast::http::request<boost::beast::http::string_body>& message, 
-    std::string contenttype);
+  Binder<boost::beast::http::request<boost::beast::http::string_body>>::
+  BindContentType(const std::string& contenttype,
+  boost::beast::http::request<boost::beast::http::string_body>& http_req);
 
 template <>
 absl::Status 
-    Binder<boost::beast::http::request<boost::beast::http::string_body>>::SetPayload(
-    boost::beast::http::request<boost::beast::http::string_body>& message, 
-    std::string payload);
+  Binder<boost::beast::http::request<boost::beast::http::string_body>>::
+  BindDataStructured(const std::string& payload,
+  boost::beast::http::request<boost::beast::http::string_body>& http_req);
+
+// _____ Operations used in Bind Binary _____
+template <>
+absl::Status 
+  Binder<boost::beast::http::request<boost::beast::http::string_body>>::
+  BindMetadata(const std::string& key, 
+  const io::cloudevents::v1::CloudEvent_CloudEventAttribute& val,
+  boost::beast::http::request<boost::beast::http::string_body>& http_req);
 
 template <>
-absl::StatusOr<boost::beast::http::request<boost::beast::http::string_body>> 
-    Binder<boost::beast::http::request<boost::beast::http::string_body>>::BindBinary(
-    io::cloudevents::v1::CloudEvent& cloud_event);
+absl::Status 
+  Binder<boost::beast::http::request<boost::beast::http::string_body>>::
+  BindDataBinary(const std::string& bin_data,
+  boost::beast::http::request<boost::beast::http::string_body>& http_req);
+
+template <>
+absl::Status 
+  Binder<boost::beast::http::request<boost::beast::http::string_body>>::
+  BindDataText(const std::string& text_data,
+  boost::beast::http::request<boost::beast::http::string_body>& http_req);
+
+// template <>
+// absl::StatusOr<boost::beast::http::request<boost::beast::http::string_body>> 
+//   Binder<boost::beast::http::request<boost::beast::http::string_body>>::BindBinary(
+//   io::cloudevents::v1::CloudEvent& cloud_event);
 
 } // binding
 } // cloudevents
