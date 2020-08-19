a939ff43f54eaf01e33819ea11e377382e8590ff
Hongxiang Jiang <hxjiang@google.com>
2020-08-19 23:42:39 +0000

    chaneg include from external/com_google_googleapis/google to google

--- v1/protocol_binding/pubsub_binder.h
+++ v1/protocol_binding/pubsub_binder.h
@@ -1,7 +1,7 @@
 #ifndef CLOUDEVENTSCPPSDK_V1_BINDING_PUBSUBBINDER_H
 #define CLOUDEVENTSCPPSDK_V1_BINDING_PUBSUBBINDER_H
 
-#include "external/com_google_googleapis/google/pubsub/v1/pubsub.pb.h"
+#include "google/pubsub/v1/pubsub.pb.h"
 #include "v1/protocol_binding/binder.h"
 
 namespace cloudevents {
